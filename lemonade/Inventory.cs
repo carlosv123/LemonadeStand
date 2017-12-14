43bbba7f359f2da8df45f6b38117118f0993db98
carlosv123 <vasquezcarlos1996@gmail.com>
2017-12-13 22:33:11 -0800

    added lemon add,remove list

--- lemonade/Inventory.cs
+++ lemonade/Inventory.cs
@@ -8,5 +8,38 @@ namespace lemonade
 {
     public  class Inventory
     {
+        List<Lemon> lemons = new List<Lemon>();
+        List<Ice> ices = new List<Ice>();
+        List<Sugar> sugars = new List<Sugar>();
+        List<Cup> cups = new List<Cup>();
+
+
+        public Inventory()
+        {
+
+        }
+
+        public List<Lemon> GetLemonList
+        {
+            get
+            {
+                return lemons;
+            }
+        }
+        public void AddLemon()
+        {
+            Lemon lemon = new Lemon();
+            lemons.Add(lemon);
+        }
+        public void RemoveLemon()
+        {
+            lemons.RemoveAt(0);
+        }
+
+
+
+
+
+
     }
 }

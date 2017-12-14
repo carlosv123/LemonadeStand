1480bb234b0f495d962488879196857be7a68f3a
carlosv123 <vasquezcarlos1996@gmail.com>
2017-12-13 22:47:32 -0800

    add, remove methods for ingredients

--- lemonade/Inventory.cs
+++ lemonade/Inventory.cs
@@ -36,7 +36,56 @@ namespace lemonade
             lemons.RemoveAt(0);
         }
 
+        public List<Ice> GetIceList
+        {
+            get
+            {
+                return ices;
+            }
+        }
+        public void AddIce()
+        {
+            Ice ice = new Ice();
+            ices.Add(ice);
+        }
+        public void RemoveIce()
+        {
+            lemons.RemoveAt(0);
+        }
 
+        public List<Sugar> GetSugarList
+        {
+           get
+            {
+                return sugars;
+            }
+        }
+        public void AddSugar()
+        {
+            Sugar sugar = new Sugar();
+            sugars.Add(sugar);
+        }
+        public void RemoveSugar()
+        {
+            sugars.RemoveAt(0);
+        }
+
+        public List<Cup> GetCupList
+        {
+            get
+            {
+                return cups;
+            }
+        }
+        public void AddCups()
+        {
+            Cup cup = new Cup();
+            cups.Add(cup);
+        }
+        public void RemoveCup()
+        {
+            cups.RemoveAt(0);
+        }
 
 
 

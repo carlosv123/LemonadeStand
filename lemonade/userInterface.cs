97ef21f7c5bf892acc0c8beb506093bd01bb5729
carlosv123 <vasquezcarlos1996@gmail.com>
2017-12-14 16:37:31 -0800

    userinterface welcome message

--- lemonade/userInterface.cs
+++ lemonade/userInterface.cs
@@ -8,5 +8,18 @@ namespace lemonade
 {
     public class userInterface
     {
+        Player player = new Player();
+        Weather weather = new Weather();
+        Store store = new Store();
+        List<Customer> customers = new List<Customer>();
+
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome, You have 7 days to go make that money! Go!");
+            Console.ReadLine();
+        }
+        
+            
+        
     }
 }

5a84b40d75a3c9211a0ef461ae54e80d4eb25478
carlosv123 <vasquezcarlos1996@gmail.com>
2017-12-14 16:13:07 -0800

    customer timer

--- lemonade/Customer.cs
+++ lemonade/Customer.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lemonade
 {
      public class Customer
     {
+        private double thirst;
+
+        public Customer()
+        {
+            
+        }
+
+        public void GetTimer()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Thread.Sleep(50);
+            stopwatch.Stop();
+            Random random = new Random();
+            
+
+        }
+        
+
     }
 }
